4cde334cf8c561fe1eb54e32e1943b15c2f04899
Qingfeng Cai <francois_cai@hotmail.com>
2012-10-17 17:17:20 +0800

    deploy on roadclouding

--- utils/export_static_roads.rb
+++ utils/export_static_roads.rb
@@ -4,7 +4,8 @@ require 'json'
 
 #$export_file = "static_roads-v2-frozen-20120822.json"
 #$export_file = "static_roads-v2-frozen-20120823.json"
-$export_file = "static_roads-v2-frozen-20120907.json"
+#$export_file = "static_roads-v2-frozen-20120907.json"
+$export_file = "static_roads-v2-frozen-20121016.json"
 
 File.open($export_file, "w") do |file|
 	file.puts StaticRoad.all.to_json
