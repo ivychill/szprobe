f662737a99ea258bd0716860f6b773babbdf9799
Qingfeng Cai <francois_cai@hotmail.com>
2012-10-18 11:41:53 +0800

    bug fixed

--- utils/worker_services.sh
+++ utils/worker_services.sh
@@ -5,7 +5,7 @@ fi
 
 echo "this script has to be executed within the app_home, waiting 3 seconds to continue or press control-c to stop..."
 sleep 3
-APP_HOME=/home/caiqingfeng/webapps/szprobe2/current
+APP_HOME=/home/www/szprobe2/current
 if [ $1 == "development" ]; then
   APP_HOME=${HOME}/Desktop/workspace/szprobe-2.x
 fi
